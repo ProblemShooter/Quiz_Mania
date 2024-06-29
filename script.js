da16e4dd2be4d3e5ae9deaaaf1c48ae0c3499333
HackeTheCode <aadityajauhari01@gmail.com>
2024-06-29 17:31:26 +0530

    updated js file

---
+++ script.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+        question: "Defination of OOPS",
+        answers: [
+            { text: "Ortho Ora Pieces system", correct: false },
+            { text: "Out of Pocket's", correct: false },
+            { text: "Object oriented Programming System", correct: true },
+            { text: "Opeta Organised Pascal's System", correct: false }
+        ]
+    },
+    {
+        question: "Which is the best programming language in the world ?",
+        answers: [
+            { text: "Java", correct: false },
+            { text: "Python", correct: false },
+            { text: "C", correct: false },
+            { text: "Javascript", correct: true }
+        ]
+    },
+    {
+        question: "who is the founder of C language",
+        answers: [
+            { text: " Ada Lovelace", correct: false },
+            { text: " James Gosling", correct: false },
+            { text: "Dennis Ritchie", correct: true },
+            { text: "Ramsus Lerdorf", correct: false }
+        ]
+    },
+    {
+        question: "Who created Pearl Programming language ?",
+        answers: [
+            { text: "Robe Pike", correct: false },
+            { text: "Larry Wall", correct: true },
+            { text: "PDon Dyen", correct: false },
+            { text: "Mikey Storkes", correct: false }
+        ]
+    }
+]
